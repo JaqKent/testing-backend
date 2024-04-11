0a94c72af5c18e546ece897279c1167847dee0c3
Luis Ramirez <jaq.kent@gmail.com>
2024-04-11 10:34:31 -0300

    minutas

--- routes/changes.js
+++ routes/changes.js
@@ -2,19 +2,11 @@ const express = require('express');
 const router = express.Router();
 const cambiosController = require('../controller/ChangesController');
 
-router.post('/incidencia/cambio', cambiosController.registrarCambioIncidencia);
-
 router.get('/incidencia/:id/cambios', cambiosController.obtenerCambiosIncidencia);
-
-router.post('/ventana/cambio', cambiosController.registrarCambioVentana);
 
 router.get('/ventana/:id/cambios', cambiosController.obtenerCambiosVentana);
 
-router.post('/:incidenciaId/:commentId/cambio', cambiosController.registrarCambioCommentsIncidencia);
-
 router.get('/:incidenciaId/:commentId/cambios', cambiosController.obtenerCambiosCommentsIncidencia);
-
-router.post('/:ventanaId/:commentId/cambio', cambiosController.registrarCambioCommentsVentana);
 
 router.get('/:ventanaId/:commentId/cambios', cambiosController.obtenerCambiosCommentsVentana);
 
